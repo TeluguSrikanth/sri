317dc50daac8a63474237907b04445be8136eacd
telugu srikanth <sri.tech81@gmail.com>
2023-09-19 13:13:53 +0530

    ffff

---
+++ src/main/java/com/ihub/www/ShajiyaApplication.java
@@ -0,0 +1,16 @@
+package com.ihub.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShajiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShajiyaApplication.class, args);
+	
+	System.out.println(" hello");
+	
+	}
+
+}
